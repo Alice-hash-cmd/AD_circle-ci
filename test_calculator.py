eb04bfb0bb26187ebf549340d346d3348788b273
Alice Thapa <Alice2.Thapa@live.uwe.ac.uk>
2026-04-29 07:35:41 +0545

    commit

--- test_calculator.py
+++ test_calculator.py
@@ -4,7 +4,7 @@ from calculator import add, subtract, multiply, divide
 class TestCalculator(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(2, 3), 6)
 
     def test_subtract(self):
         self.assertEqual(subtract(5, 3), 2)
